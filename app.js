f9ea47529e14cd61415153e651b65aa0eec80d7b
Aaron Capellino <acapellinogodoy@gmail.com>
2025-03-15 21:52:50 -0500

    Segunda actualizacion, agregando amigos a la lista

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigo = [];
+let nombresAmigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    
+    if (!nombreAmigo) {
+        alert("Ingresa un nombre");
+        return;
+    }
+
+    nombresAmigos.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus()
+}
+
+function renderizarAmigos() {}
 
